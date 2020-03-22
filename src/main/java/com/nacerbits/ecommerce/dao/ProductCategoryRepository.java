09bcf30c0599fe3a83f8e54bf5ef8030dae2eb33
Nacer Bouchekhima <nbouchek@gmail.com>
2020-03-22 10:50:06 +0000

    Create REST APIs with Spring Data JPA Repositories and Spring DATA REST.

---
+++ src/main/java/com/nacerbits/ecommerce/dao/ProductCategoryRepository.java
@@ -0,0 +1,12 @@
+package com.nacerbits.ecommerce.dao;
+
+import com.nacerbits.ecommerce.entitiy.ProductCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin("http://localhost:4200")
+@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product_category")
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
+
+}
