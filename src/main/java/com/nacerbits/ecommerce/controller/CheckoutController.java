a3b5af7e64d36b0f854d4609a7dc8505334942bb
Nacer Bouchekhima <nbouchek@gmail.com>
2020-12-20 18:35:48 +0000

    Create controller

---
+++ src/main/java/com/nacerbits/ecommerce/controller/CheckoutController.java
@@ -0,0 +1,29 @@
+package com.nacerbits.ecommerce.controller;
+
+import com.nacerbits.ecommerce.dto.Purchase;
+import com.nacerbits.ecommerce.dto.PurchaseResponse;
+import com.nacerbits.ecommerce.service.CheckoutService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:4200")
+@RestController
+@RequestMapping("/api/checkout")
+public class CheckoutController {
+    private CheckoutService checkoutService;
+
+    @Autowired
+    public CheckoutController(CheckoutService checkoutService) {
+        this.checkoutService = checkoutService;
+    }
+
+    @PostMapping("/purchase")
+    public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
+
+
+
+
+
+        return checkoutService.placeOrder(purchase);
+    }
+}
